0b0ed551501cbbedceee16db7fbf52be6888553a
Janne Lindstrom <j.lindstrom@live.co.uk>
2015-04-15 13:49:05 +0200

    Renamed Media.py to media.py

---
+++ media.py
@@ -0,0 +1,9 @@
+import webbrowser as browser
+
+class Movie ( ) :
+	def __init__ ( self , title , desc , poster , trailer ) :
+		
+		self.title = title
+		self.desc = desc
+		self.poster = poster
+		self.trailer = trailer
